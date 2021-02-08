80ab365ec4a001c90c12532cb127021d1137f4af
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-02-08 13:30:19 -0600

    setup

---
+++ 1- template/sketch.js
@@ -0,0 +1,16 @@
+// tittle
+// date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+ 
+}
