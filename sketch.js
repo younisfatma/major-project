2d84992ee85b4ff8004e58f9c563acad1d098cfd
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-02-10 14:12:31 -0600

    added collision

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 // Fatma Younis 
 // Computer Science 30
 // Mr. Schellenberg
-// Grid assignment
+// Major Project
 
-// Extra for experts:
-// obstical collision
-// character skins (images)
-// automatically moving object in grid
-// sounds
-// fonts
 
-;
+// Array for start screen
+let start = {x: 0, y: 0, isAlive: true,};
+
+// Used to tell the time since the game started
+let seconds = 0;
+let minutes= 0;
+let lastSecond = 0;
+let lastAddedFood = 0;
+let timeLastAddeddFood = 0;
+let millisSinceGameStarted;
 
 // Images
 let bgImg, evilImg, hamsterImg, whaleImg, zombieImg, skeletonImg;
@@ -24,24 +27,24 @@ let millisSinceLossSoundPlayed = 0;
 // Font
 let font;
 
-// function preload(){
-//   // Images
-//   bgImg = loadImage("assets/bg.png");
-//   evilImg = loadImage ("assets/evil.png");
-//   hamsterImg = loadImage ("assets/hamster.png");
-//   zombieImg = loadImage ("assets/zombie.png");
-//   whaleImg = loadImage ("assets/whale.png");
-//   skeletonImg = loadImage ("assets/skeleton.png");
-//   images.push(evilImg, hamsterImg, zombieImg, whaleImg, skeletonImg);
+function preload(){
+  // Images
+  bgImg = loadImage("assets/bg.png");
+  evilImg = loadImage ("assets/evil.png");
+  hamsterImg = loadImage ("assets/hamster.png");
+  zombieImg = loadImage ("assets/zombie.png");
+  whaleImg = loadImage ("assets/whale.png");
+  skeletonImg = loadImage ("assets/skeleton.png");
+  images.push(evilImg, hamsterImg, zombieImg, whaleImg, skeletonImg);
 
-//   // Sounds
-//   bgSound = loadSound("assets/backgroundMusic.mp3");
+  //   // Sounds
+  //   bgSound = loadSound("assets/backgroundMusic.mp3");
 //   lossSound = loadSound("assets/loss.mp3");
 //   victorySound = loadSound("assets/Victory.mp3");
 
 //   // Font
 //   font = loadFont("assets/ARCADECLASSIC.TTF");
-// }
+}
 
 let thePlayer, theObstacles;
 let theFoods= [];
@@ -49,7 +52,6 @@ let theFoods= [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   thePlayer = new Player();
-  theFood = new Player();
   theObstacles = new Obstacle;
   // Play sound
   // bgSound.play();
@@ -63,30 +65,49 @@ function windowResized() {
 function draw() {
   background(255);
 
-  for (let food of theFoods){
-    food.display();
+  for (let i = theFoods.length-1; i > 0; i--){
+    theFoods[i].display();
+    theFoods[i].hit();
+    if(theFoods[i].eaten){ //only eat food that are smaller
+      theFoods.splice(i, 1);
+      thePlayer.diameter+=2;
+    }
   }
 
   thePlayer.display();
   thePlayer.move();
 
   theObstacles.display();
+
+  theObstacles.collide();
+  
 }
 
-
+function time(){
+  if (millis() - millisSinceGameStarted - lastSecond >= 1){
+    seconds +=1;
+    lastSecond += 1000;
+    if (seconds >= 60){
+      seconds = 0;
+      minutes +=1;
+    }
+  }
+  fill("white");
+  text("Time  "+ minutes + ":"+ seconds, 80, 30); 
+}
 
 // Starts the game and controls color of the ball
 function mousePressed() {
   let newFood = new Food;
-  theFoods.push(newFood);
+  theFoods.push(newFood);//make new food spawn based on time
 
-  // Controls what skin to display
-  // if (imagenumber < 4){
-  //   imagenumber+=1;
-  // }
-  // else{
-  //   imagenumber=0;
-  // }
+  //Controls what skin to display
+  if (thePlayer.imagenumber < 4){
+    this.imagenumber+=1;
+  }
+  else{
+    imagenumber=0;
+  }
 }
 
 class Player{
@@ -94,38 +115,23 @@ class Player{
     this.x= width/2;
     this.y = height/2;
     this.radius = 20;
+    this.diameter = this.radius*2;
     this.speed = 5;
-    this.color = "pink";
-    //color(255,255,255,0);
+    this.color = color(255,255,255,0);
     this.imagenumber = 0;
-    
   }
 
   display(){
     noStroke();
     fill(this.color);
-    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
     ellipseMode(CENTER);
 
     // Character skins
-    // imageMode(CENTER);
-    // image(images[this.imagenumber], this.x, this.y, this.radius*2, this.radius*2 );
+    imageMode(CENTER);
+    image(images[this.imagenumber], this.x, this.y, this.diameter, this.diameter );
   }
   
-
-  // if (player.x - player.radius < 0) {
-  //   player.x = player.radius;
-  // }
-  // if (player.x + player.radius > width) {
-  //   player.x = width - player.radius;
-  // }
-  // if (player.y - player.radius < 0) {
-  //   player.y = player.radius;
-  // }
-  // if (player.y + player.radius > height) {
-  //   player.y = height - player.radius;
-  // }
-
   move(){
     if (keyIsDown(65)) { //a
       this.x -= this.speed;
@@ -141,7 +147,8 @@ class Player{
     if (keyIsDown(83)) { //d
       this.y += this.speed;
     }
-    
+
+    //prevent the player from flying off the screen
     if (this.x - this.radius < 0) {
       this.x = this.radius;
     }
@@ -171,37 +178,43 @@ class Player{
     }
   }
   
-  width(){
+  radius(){
     this.radius += 3;
   }
 }
 
 class Food{
-  constructor(){
+  constructor(shortRadius, largeRadius){
     this.x = random(0, width);
     this.y = random(0, height);
     this.radius = random(5, 20);
-    this.color = "yellow";
-    //color(random(255), random(255), random(255));
+    this.color = color(random(255), random(255), random(255));
     this.eaten = false;
   }
   
   display(){
     noStroke();
     fill(this.color);
-    circle(this.x, this.y, this.radius * 2);
+    ellipseMode(CENTER);
+    ellipse(this.x, this.y, this.radius*2, this.radius*2);
   }
 
-  // checkCollision(){
-  //   this.eaten = collideCircleCircle(player.x, player.y, player.radius, this.x, this.y, this.radius);
-  // }
+  hit(){
+    this.eaten = collideCircleCircle(thePlayer.x, thePlayer.y, thePlayer.diameter, this.x, this.y, this.radius);
+    if (this.eaten){
+      console.log("yum");
+    }
+  }
 }
 
 class Obstacle{
   constructor(){
-    this.x = random(0, width);
-    this.y = random(0, height);
     this.length = random(50, 100);
+    this.x = random(0, width-this.length);
+    this.y = random(0, height-this.length);
+    this.dx = random(1, 5);
+    this.dy = random(1, 5);
+    this.hit= false;
     this.color = "lime";
   }
 
@@ -212,13 +225,22 @@ class Obstacle{
   }
 
   // move(){
-    
+  //   if (this.x-this.length >= 0 && this.x + this.length <= width){
+  //     this.x += this.dx;
+  //   }
+  //   else if (this.y-this.length>=0 && this.y+this.length<=height){
+  //     this.y += this.dy;
+  //   }
   // }
 
-  // hit(){
-  //   collideRectCircle(this.x, this.y, this.length, this.length, player.x, player.y, player.radius);
-  // }
-  
-
+  collide(){
+    this.hit= collideRectCircle(this.x, this.y, this.length, this.length, thePlayer.x, thePlayer.y, thePlayer.radius);
+    if (this.hit){
+      console.log("ouch");
+    }
+    if(this.hit && this.length < thePlayer.diameter && thePlayer.diameter >= 40){
+      thePlayer.diameter-=5;
+    }
+  }
 }
 
